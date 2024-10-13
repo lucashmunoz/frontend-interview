cede1ffd9e560e7e3086a22928e5a5abcc79c938
lucashmunoz <lucashmunoz@gmail.com>
2024-10-13 03:26:21 +0000

    feat: add i18next mock for tests

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,7 +1,23 @@
-import { afterEach } from "vitest";
+import { afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { server } from "./mocks/server";
+
+vi.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {})
+      }
+    };
+  },
+  initReactI18next: {
+    type: "3rdParty",
+    init: () => {}
+  }
+}));
 
 // Runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
